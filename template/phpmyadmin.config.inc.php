1d41456451f88c42d281ba5294ed8841f43e0d92
ubtind <ubtind@gmail.com>
2026-03-05 06:36:10 -0800

    Add auto-generated phpMyAdmin config file

---
+++ template/phpmyadmin.config.inc.php
@@ -0,0 +1,13 @@
+<?php
+/* Auto-generated config */
+$i = 0;
+$i++;
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+$cfg['Servers'][$i]['host'] = '127.0.0.1';
+$cfg['Servers'][$i]['port'] = {{MYSQL_PORT}};
+$cfg['Servers'][$i]['user'] = 'root';
+$cfg['Servers'][$i]['password'] = '';
+$cfg['blowfish_secret'] = '{{BLOWFISH_SECRET}}';
+$cfg['UploadDir'] = '';
+$cfg['SaveDir'] = '';
+return $cfg;
